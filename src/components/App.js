412088905ab5d1f0b0bee409aa07f014590386af
EyeRoe <EyeRoe@users.noreply.github.com>
2018-04-02 15:00:41 -0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Read Me</h1>
+        </header>
+        <p>Put stuff here please</p>
+      </div>
+    );
+  }
+}
+
+export default App;
