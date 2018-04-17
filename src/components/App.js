c245ea9b5979236ff8b79c98297dd1a8824b5628
EyeRoe <EyeRoe@users.noreply.github.com>
2018-04-16 20:08:41 -0700

    added react router

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,8 @@ import Content from './Content.js';
 import LogIn from './LogIn.js';
 import SignUp from './SignUp.js';
 import ReadingList from './ReadingList'
+import { Switch, Route } from 'react-router-dom'
+
 
 
 class App extends Component {
@@ -14,21 +16,42 @@ class App extends Component {
   }
 
   setResponseList = (data) => {
-    this.setState({responseList: data})
+    this.setState({ responseList: data })
   }
-
+/*
   render() {
     return (
       <div className="App">
         <Header text="Read Me" />
-        <LogIn/>
-        <SignUp/>
-        <ReadingList />
-        <Form setResponseList={this.setResponseList}/>
-        <Content responseList={this.state.responseList}/>
+        <Switch>
+          <LogIn />
+          <SignUp />
+          <ReadingList />
+          <Form setResponseList={this.setResponseList} />
+          <Content responseList={this.state.responseList} />
+        </Switch>
       </div>
     );
   }
+*/
+render() {
+  return (
+    <div className="App">
+      <Header text="Read Me" />
+      <Switch>
+        <Route path='/signup' component={SignUp}/>
+        <Route path='/login' component={LogIn}/>
+        <Route path='/readinglist' component={ReadingList}/>
+        <Route exact path='/' render={(props) => (
+          <div>
+            <Form setResponseList={this.setResponseList} />
+            <Content responseList={this.state.responseList} />
+          </div>
+        )}/>
+      </Switch>
+    </div>
+  );
+}
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
 
 class Header extends Component {
 
@@ -6,6 +8,10 @@ class Header extends Component {
     return (
       <div className="App-header">
         <h1>{this.props.text}</h1>
+        <button><Link to='/'>Home</Link></button>
+        <button><Link to='/signup'>Sign up</Link></button>
+        <button><Link to='/login'>login</Link></button>
+        <button><Link to='/readinglist'>Reading List</Link></button>
       </div>
     )
   }
